20477396c1316bcfa9adc381960b951361a83020
samie123 <53871480+samie123@users.noreply.github.com>
2019-08-15 18:51:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,32 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "{";
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var prefix = "$";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};
 });
 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`starcraft`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login(NjExNjEyNzA0MDQxNDY3OTE2.XVWW-A.qtRUw8p0-2TrlakVFPHy-RspIN0);
